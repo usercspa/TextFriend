a9b0fcfc99fd792ab08496e3d455ccc62791c827
usercspa <spacchbk@gmail.com>
2020-11-29 07:38:00 +0000

    2 files modified, 1 added

--- sendSMS.js
+++ sendSMS.js
@@ -6,5 +6,6 @@ const cilent = require('twilio')(accountSid, authToken)
 cilent.messages.create({
     to: process.env.MY_PHONE_NUMBER,
     from: '+447723499643',
-    body: 'You are paired up with '
+    body: 'Matching is completed, our volunteer will get in touch with you shortly:)',
 })
+.then((message) => console.log(message.sid));
